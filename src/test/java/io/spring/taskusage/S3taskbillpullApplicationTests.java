6a10fd78b9b8ebfe11f9e56341c300914fc754d1
Glenn Renfro <grenfro@pivotal.io>
2019-04-09 13:51:49 -0400

    Updated to output to database

--- src/test/java/io/spring/taskusage/S3taskbillpullApplicationTests.java
+++ src/test/java/io/spring/taskusage/S3taskbillpullApplicationTests.java
@@ -16,28 +16,30 @@
 
 package io.spring.taskusage;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import io.spring.taskusage.configuration.S3Processor;
 import io.spring.taskusage.configuration.TaskProcessorConfiguration;
-import org.junit.Before;
-import org.junit.Rule;
+import io.spring.taskusage.configuration.Usage;
+import javax.sql.DataSource;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.cloud.aws.core.io.s3.PathMatchingSimpleStorageResourcePatternResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.StreamUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -49,48 +51,56 @@ public class S3taskbillpullApplicationTests {
 
 	private static final String FILE_NAME = "test.json";
 
-	private String tempDir;
-
-	@Rule
-	public TemporaryFolder folder = new TemporaryFolder();
-
-	@Before
-	public void tearDown() throws IOException{
-		File file = folder.newFolder();
-		this.tempDir = file.getAbsolutePath() + "/";
-	}
-
 	@Test
 	public void testRepository() {
 		ApplicationContextRunner applicationContextRunner = new ApplicationContextRunner()
-				.withPropertyValues("io.spring.outputDirectory=" + this.tempDir)
 				.withConfiguration(
-						AutoConfigurations.of(TaskProcessorConfiguration.class,
+						AutoConfigurations.of(EmbeddedDataSourceConfiguration.class, TaskProcessorConfiguration.class,
 								TestConfiguration.class));
-
 		applicationContextRunner.run((context) -> {
+			DataSource dataSource = context.getBean(DataSource.class);
 			S3Processor s3Processor = context.getBean(S3Processor.class);
+			initializeDatabase(dataSource);
 			s3Processor.processResources();
-			FileSystemResource fileSystemResource = new FileSystemResource(tempDir + FILE_NAME);
-			assertThat(fileSystemResource.exists()).isTrue();
-			String result = StreamUtils.copyToString( fileSystemResource.getInputStream(), StandardCharsets.UTF_8);
-			assertThat(result).isEqualTo("[{\"id\":\"1\",\"firstName\":\"jane\",\"lastName\":\"doe\",\"minutes\":\"500\",\"dataUsage\":\"1000\"}]");
+			List<Usage> usages = getResultsFromDB(dataSource);
+			assertThat(usages.size()).isEqualTo(1);
+			assertThat(usages.get(0).getId()).isEqualTo(1);
+			assertThat(usages.get(0).getFirstName()).isEqualTo("jane");
 		});
 	}
+
+	public void initializeDatabase(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("CREATE TABLE bill_usage ( id int, first_name varchar(50), last_name varchar(50), minutes int, data_usage int)");
+	}
+
+	public List<Usage> getResultsFromDB(DataSource dataSource) {
+			NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+			return jdbcTemplate.query("select ID, FIRST_NAME, LAST_NAME, MINUTES, DATA_USAGE FROM BILL_USAGE", new UsageRowMapper());
+	}
+
 
 	@Configuration
 	public static class TestConfiguration {
 
-		private Resource testfile = new ClassPathResource(FILE_NAME);
-
 		@Bean
 		public PathMatchingSimpleStorageResourcePatternResolver pathMatchingSimpleStorageResourcePatternResolver() throws IOException {
 			PathMatchingSimpleStorageResourcePatternResolver pathMatchingResourceResolver = mock(PathMatchingSimpleStorageResourcePatternResolver.class);
-			Resource[] resources = {this.testfile};
+			Resource[] resources = {new ClassPathResource(FILE_NAME)};
 			when(pathMatchingResourceResolver.getResources(anyString())).thenReturn(resources);
 
 			return pathMatchingResourceResolver;
 
 		}
+	}
+
+	private final class UsageRowMapper implements RowMapper<Usage> {
+		@Override
+		public Usage mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new Usage(rs.getLong("id"),
+					rs.getString("FIRST_NAME"), rs.getString("LAST_NAME"),
+					rs.getLong("MINUTES"), rs.getLong("DATA_USAGE"));
+		}
+
 	}
 }
